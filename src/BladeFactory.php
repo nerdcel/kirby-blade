344767e860c86b6352a2904c6322e41a5860e45a
Lukas Leitsch <lukas@leitsch.org>
2022-01-13 07:36:23 +0100

    new setup to allow x-component syntax

---
+++ src/BladeFactory.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Afbora;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\View;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\View\Engines\CompilerEngine;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Factory;
+use Illuminate\View\FileViewFinder;
+
+class BladeFactory
+{
+    protected Factory $viewFactory;
+
+    public function __construct(array $pathsToTemplates, string $pathToCompiledTemplates)
+    {
+        $container = App::getInstance();
+
+        // we have to bind our app class to the interface
+        // as the blade compiler needs the `getNamespace()` method to guess Blade component FQCNs
+        $container->instance(Application::class, $container);
+
+        // Dependencies
+        $filesystem = new Filesystem;
+        $eventDispatcher = new Dispatcher($container);
+
+        // Create View Factory capable of rendering PHP and Blade templates
+        $viewResolver = new EngineResolver;
+        $bladeCompiler = new BladeCompiler($filesystem, $pathToCompiledTemplates);
+
+        $viewResolver->register('blade', fn() => new CompilerEngine($bladeCompiler));
+
+        $viewFinder = new FileViewFinder($filesystem, $pathsToTemplates);
+        $this->viewFactory = new Factory($viewResolver, $viewFinder, $eventDispatcher);
+        $this->viewFactory->setContainer($container);
+        Facade::setFacadeApplication($container);
+        $container->instance(\Illuminate\Contracts\View\Factory::class, $this->viewFactory);
+        $container->alias(
+            \Illuminate\Contracts\View\Factory::class,
+            (new class extends View {public static function getFacadeAccessor() { return parent::getFacadeAccessor(); }
+            })::getFacadeAccessor()
+        );
+        $container->instance(BladeCompiler::class, $bladeCompiler);
+        $container->alias(
+            BladeCompiler::class,
+            (new class extends \Illuminate\Support\Facades\Blade {
+                public static function getFacadeAccessor() { return parent::getFacadeAccessor(); }
+            })::getFacadeAccessor()
+        );
+    }
+
+    public function getViewFactory(): Factory
+    {
+        return $this->viewFactory;
+    }
+}
--- src/Template.php
+++ src/Template.php
@@ -2,8 +2,9 @@
 
 namespace Afbora;
 
-use Afbora\Blade\Blade;
 use Exception;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\View;
 use Kirby\Cms\App as Kirby;
 use Kirby\Cms\Template as KirbyTemplate;
 use Kirby\Toolkit\F;
@@ -13,32 +14,17 @@ use voku\helper\HtmlMin;
 
 class Template extends KirbyTemplate
 {
-    public static $data = [];
-    protected $blade;
-    protected $views;
-    protected $defaultType;
-    protected $name;
-    protected $templates;
-    protected $type;
+    protected BladeFactory $blade;
 
     public function __construct(Kirby $kirby, string $name, string $type = 'html', string $defaultType = 'html')
     {
-        $this->templates = $this->getPathTemplates();
-        $this->views = $this->getPathViews();
+        parent::__construct($name, $type, $defaultType);
 
-        $this->name = strtolower($name);
-        $this->type = $type;
-        $this->defaultType = $defaultType;
-
-        $this->setViewDirectory();
+        $this->blade = new BladeFactory([$this->getPathTemplates()], $this->getPathViews());
     }
 
     protected function getPathTemplates()
     {
-        if (!is_null($this->templates)) {
-            return $this->templates;
-        }
-
         $optionPath = option('afbora.blade.templates');
 
         if ($optionPath !== null && is_dir($optionPath)) {
@@ -65,25 +51,18 @@ class Template extends KirbyTemplate
         return $path;
     }
 
-    public function setViewDirectory()
-    {
-        if (!file_exists($this->views)) {
-            Dir::make($this->views);
-        }
-    }
-
     public function render(array $data = []): string
     {
         if ($this->isBlade() && $this->hasDefaultType() === true) {
-            $this->blade = new Blade(
-                $this->templates,
-                $this->views
-            );
-
             $this->setDirectives();
             $this->setIfStatements();
 
-            $html = $this->blade->make($this->name, $data)->render();
+            View::share('kirby', $data['kirby']);
+            View::share('site', $data['site']);
+            View::share('pages', $data['pages']);
+            View::share('page', $data['page']);
+
+            $html = $this->blade->getViewFactory()->make($this->name, $data)->render();
         } else {
             $html = Tpl::load($this->file(), $data);
         }
@@ -115,15 +94,15 @@ class Template extends KirbyTemplate
 
     protected function setDirectives()
     {
-        $this->blade->compiler()->directive('asset', function (string $path) {
+        Blade::directive('asset', function (string $path) {
             return "<?php echo asset($path) ?>";
         });
 
-        $this->blade->compiler()->directive('csrf', function () {
+        Blade::directive('csrf', function () {
             return "<?php echo csrf() ?>";
         });
 
-        $this->blade->compiler()->directive('css', function (string $url, array $options = []) {
+        Blade::directive('css', function (string $url, array $options = []) {
             if ($options) {
                 return "<?php echo css($url, $options) ?>";
             }
@@ -131,7 +110,7 @@ class Template extends KirbyTemplate
             return "<?php echo css($url) ?>";
         });
 
-        $this->blade->compiler()->directive('e', function (mixed $condition, mixed $value, mixed $alternative = null) {
+        Blade::directive('e', function (mixed $condition, mixed $value, mixed $alternative = null) {
             if ($alternative) {
                 return "<?php echo e($condition, $value, $alternative) ?>";
             }
@@ -139,7 +118,7 @@ class Template extends KirbyTemplate
             return "<?php echo e($condition, $value) ?>";
         });
 
-        $this->blade->compiler()->directive('get', function (string $key, mixed $default = null) {
+        Blade::directive('get', function (string $key, mixed $default = null) {
             if ($default) {
                 return "<?php echo get($key, $default) ?>";
             }
@@ -147,7 +126,7 @@ class Template extends KirbyTemplate
             return "<?php echo get($key) ?>";
         });
 
-        $this->blade->compiler()->directive('gist', function (string $url, string $file = null) {
+        Blade::directive('gist', function (string $url, string $file = null) {
             if ($file) {
                 return "<?php echo gist($url, $file) ?>";
             }
@@ -155,7 +134,7 @@ class Template extends KirbyTemplate
             return "<?php echo gist($url) ?>";
         });
 
-        $this->blade->compiler()->directive('h', function (string $string, bool $keepTags = false) {
+        Blade::directive('h', function (string $string, bool $keepTags = false) {
             if ($keepTags) {
                 return "<?php echo h($string, $keepTags) ?>";
             }
@@ -163,7 +142,7 @@ class Template extends KirbyTemplate
             return "<?php echo h($string) ?>";
         });
 
-        $this->blade->compiler()->directive('html', function (string $string, bool $keepTags = false) {
+        Blade::directive('html', function (string $string, bool $keepTags = false) {
             if ($keepTags) {
                 return "<?php echo html($string, $keepTags) ?>";
             }
@@ -171,7 +150,7 @@ class Template extends KirbyTemplate
             return "<?php echo html($string) ?>";
         });
 
-        $this->blade->compiler()->directive('js', function ($url, array $options = []) {
+        Blade::directive('js', function ($url, array $options = []) {
             if ($options) {
                 return "<?php echo js($url, $options) ?>";
             }
@@ -179,11 +158,11 @@ class Template extends KirbyTemplate
             return "<?php echo js($url) ?>";
         });
 
-        $this->blade->compiler()->directive('image', function (string $path) {
+        Blade::directive('image', function (string $path) {
             return "<?php echo image($path) ?>";
         });
 
-        $this->blade->compiler()->directive('kirbytag', function (mixed $type, string $value, array $attr = []) {
+        Blade::directive('kirbytag', function (mixed $type, string $value, array $attr = []) {
             if ($attr) {
                 return "<?php echo kirbytag($type, $value, $attr) ?>";
             }
@@ -191,7 +170,7 @@ class Template extends KirbyTemplate
             return "<?php echo kirbytag($type, $value) ?>";
         });
 
-        $this->blade->compiler()->directive('kirbytext', function (string $text, array $data = []) {
+        Blade::directive('kirbytext', function (string $text, array $data = []) {
             if ($data) {
                 return "<?php echo kirbytext($text, $data) ?>";
             }
@@ -199,7 +178,7 @@ class Template extends KirbyTemplate
             return "<?php echo kirbytext($text) ?>";
         });
 
-        $this->blade->compiler()->directive('kirbytextinline', function (string $text, array $data = []) {
+        Blade::directive('kirbytextinline', function (string $text, array $data = []) {
             if ($data) {
                 return "<?php echo kirbytextinline($text, $data) ?>";
             }
@@ -207,7 +186,7 @@ class Template extends KirbyTemplate
             return "<?php echo kirbytextinline($text) ?>";
         });
 
-        $this->blade->compiler()->directive('kt', function (string $text, array $data = []) {
+        Blade::directive('kt', function (string $text, array $data = []) {
             if ($data) {
                 return "<?php echo kirbytext($text, $data) ?>";
             }
@@ -215,11 +194,11 @@ class Template extends KirbyTemplate
             return "<?php echo kirbytext($text) ?>";
         });
 
-        $this->blade->compiler()->directive('markdown', function (string $text) {
+        Blade::directive('markdown', function (string $text) {
             return "<?php echo markdown($text) ?>";
         });
 
-        $this->blade->compiler()->directive('option', function (string $key, mixed $default = null) {
+        Blade::directive('option', function (string $key, mixed $default = null) {
             if ($default) {
                 return "<?php echo option($key, $default) ?>";
             }
@@ -228,7 +207,7 @@ class Template extends KirbyTemplate
         });
 
 
-        $this->blade->compiler()->directive('param', function (string $key, string $fallback = null) {
+        Blade::directive('param', function (string $key, string $fallback = null) {
             if ($fallback) {
                 return "<?php echo param($key, $fallback) ?>";
             }
@@ -236,15 +215,15 @@ class Template extends KirbyTemplate
             return "<?php echo param($key) ?>";
         });
 
-        $this->blade->compiler()->directive('size', function (mixed $value) {
+        Blade::directive('size', function (mixed $value) {
             return "<?php echo size($value) ?>";
         });
 
-        $this->blade->compiler()->directive('smartypants', function (string $text) {
+        Blade::directive('smartypants', function (string $text) {
             return "<?php echo smartypants($text) ?>";
         });
 
-        $this->blade->compiler()->directive('snippet', function (string $name, mixed $data = null) {
+        Blade::directive('snippet', function (string $name, mixed $data = null) {
             if ($data) {
                 return "<?php echo snippet($name, $data) ?>";
             }
@@ -252,11 +231,11 @@ class Template extends KirbyTemplate
             return "<?php echo snippet($name) ?>";
         });
 
-        $this->blade->compiler()->directive('svg', function (string $file) {
+        Blade::directive('svg', function (string $file) {
             return "<?php echo svg($file) ?>";
         });
 
-        $this->blade->compiler()->directive('t', function (mixed $key, string $fallback = null) {
+        Blade::directive('t', function (mixed $key, string $fallback = null) {
             if ($fallback) {
                 return "<?php echo t($key, $fallback) ?>";
             }
@@ -264,11 +243,11 @@ class Template extends KirbyTemplate
             return "<?php echo t($key) ?>";
         });
 
-        $this->blade->compiler()->directive('tc', function (mixed $key, int $count) {
+        Blade::directive('tc', function (mixed $key, int $count) {
             return "<?php echo tc($key, $count) ?>";
         });
 
-        $this->blade->compiler()->directive('twitter', function (string $username, string $text = null, string $title = null, string $class = null) {
+        Blade::directive('twitter', function (string $username, string $text = null, string $title = null, string $class = null) {
             if ($text) {
                 return "<?php echo twitter($username, $text) ?>";
             } elseif ($text && $title) {
@@ -280,7 +259,7 @@ class Template extends KirbyTemplate
             return "<?php echo twitter($username) ?>";
         });
 
-        $this->blade->compiler()->directive('u', function (string $path = null, mixed $options = null) {
+        Blade::directive('u', function (string $path = null, mixed $options = null) {
             if ($options) {
                 return "<?php echo u($path, $options) ?>";
             }
@@ -288,7 +267,7 @@ class Template extends KirbyTemplate
             return "<?php echo u($path) ?>";
         });
 
-        $this->blade->compiler()->directive('url', function (string $path = null, mixed $options = null) {
+        Blade::directive('url', function (string $path = null, mixed $options = null) {
             if ($path) {
                 return "<?php echo url($path) ?>";
             } elseif ($path && $options) {
@@ -298,7 +277,7 @@ class Template extends KirbyTemplate
             return "<?php echo url() ?>";
         });
 
-        $this->blade->compiler()->directive('video', function (string $url, array $options = [], array $attr = []) {
+        Blade::directive('video', function (string $url, array $options = [], array $attr = []) {
             if ($options) {
                 return "<?php echo video($url, $options) ?>";
             } elseif ($options && $attr) {
@@ -308,7 +287,7 @@ class Template extends KirbyTemplate
             return "<?php echo video($url) ?>";
         });
 
-        $this->blade->compiler()->directive('vimeo', function (string $url, array $options = [], array $attr = []) {
+        Blade::directive('vimeo', function (string $url, array $options = [], array $attr = []) {
             if ($options) {
                 return "<?php echo vimeo($url, $options) ?>";
             } elseif ($options && $attr) {
@@ -318,11 +297,11 @@ class Template extends KirbyTemplate
             return "<?php echo vimeo($url) ?>";
         });
 
-        $this->blade->compiler()->directive('widont', function (string $string) {
+        Blade::directive('widont', function (string $string) {
             return "<?php echo widont($string) ?>";
         });
 
-        $this->blade->compiler()->directive('youtube', function (string $url, array $options = [], array $attr = []) {
+        Blade::directive('youtube', function (string $url, array $options = [], array $attr = []) {
             if ($options) {
                 return "<?php echo youtube($url, $options) ?>";
             } elseif ($options && $attr) {
@@ -333,14 +312,14 @@ class Template extends KirbyTemplate
         });
 
         foreach ($directives = option('afbora.blade.directives', []) as $directive => $callback) {
-            $this->blade->compiler()->directive($directive, $callback);
+            Blade::directive($directive, $callback);
         }
     }
 
     protected function setIfStatements()
     {
-        foreach ($statements = option('afbora.blade.ifs', []) as $statement => $callback) {
-            $this->blade->compiler()->if($statement, $callback);
+        foreach (option('afbora.blade.ifs', []) as $statement => $callback) {
+            Blade::if($statement, $callback);
         }
     }
 
