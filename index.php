c96d5d0b10a3d4922b6fa613f23c4249ffa5cc9c
Lukas Leitsch <lukas@leitsch.org>
2022-07-01 16:14:50 +0200

    refactor to snippet class

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\View;
 use Kirby\Cms\App as Kirby;
-use Kirby\Toolkit\A;
-use Kirby\Toolkit\Tpl as Snippet;
 use Leitsch\Blade\BladeDirectives;
 use Leitsch\Blade\BladeFactory;
 use Leitsch\Blade\BladeIfStatements;
 use Leitsch\Blade\Paths;
+use Leitsch\Blade\Snippet;
 use Leitsch\Blade\Template;
 
 @include_once __DIR__ . '/vendor/autoload.php';
@@ -25,37 +23,7 @@ Kirby::plugin('leitsch/blade', [
             return new Template($kirby, $name, $contentType);
         },
         'snippet' => function (Kirby $kirby, $name, array $data = []): ?string {
-            $snippets = A::wrap($name);
-
-            foreach ($snippets as $name) {
-                $name = (string)$name;
-                $file = null;
-                $bladeFile = $kirby->root('snippets') . '/' . $name . '.' . Template::EXTENSION_BLADE;
-                $fallbackFile = $kirby->root('snippets') . '/' . $name . '.' . Template::EXTENSION_FALLBACK;
-
-                if (file_exists($bladeFile)) {
-                    // blade snippet exists
-                    $file = $bladeFile;
-                } elseif (file_exists($fallbackFile)) {
-                    // vanilla PHP snippets exists
-                    $file = $fallbackFile;
-                } else {
-                    // look for snippet from plugin
-                    $file = $kirby->extensions('snippets')[$name] ?? null;
-                }
-
-                if ($file) {
-                    break;
-                }
-            }
-
-            if (str_ends_with($file, Template::EXTENSION_BLADE)) {
-                // blade snippet
-                return View::file($file, $data)->render();
-            } else {
-                // vanilly PHP snippet
-                return Snippet::load($file, $data);
-            }
+            return (new Snippet($kirby, $name, $data))->load();
         },
     ],
     'hooks' => [
